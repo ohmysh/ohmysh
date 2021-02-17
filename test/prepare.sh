1d9201d40d75c9d3221bc20bd8dda2cd3460376e
5+1 <59787082+langonginc@users.noreply.github.com>
2021-02-17 09:41:21 +0800

    Update prepare.sh

--- test/prepare.sh
+++ test/prepare.sh
@@ -14,7 +14,7 @@ OMS_DIR='$path'
 OMS_CACHE='$path/test/cache'
 # OhMySh theme
 OMS_THEME='colorshell'
-OMS_PLUGIN=()
+OMS_PLUGIN=(helloworld )
 # OhMySh main script
 source "\$OMS_DIR/main.sh"
 EOF
