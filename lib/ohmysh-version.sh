f5605979a26395b6303cccf077b2005580131b2c
langonginc <langonginc@yeah.net>
2021-03-21 15:08:00 +0800

    Prepare to 0.0.3-PRE_9

--- lib/ohmysh-version.sh
+++ lib/ohmysh-version.sh
@@ -2,9 +2,9 @@
 # OhMySh CLI
 #   !!! You can NOT edit this file !!!
 
-OMS_VER='0.0.3-build_8'
-OMS_BUILD='8'
-OMS_PRE='build'
+OMS_VER='0.0.3-pre_9'
+OMS_BUILD='9'
+OMS_PRE='pre'
 
 OMS_CLI_VER='0.0.3-build_7'
 OMS_CLI_BUILD='7'
