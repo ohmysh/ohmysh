3d173ab0f533c72f5494ca7936333123a78186c8
langonginc <langonginc@yeah.net>
2021-05-11 16:23:21 +0800

    Update to 0.0.4-build_11

--- lib/ohmysh-version.sh
+++ lib/ohmysh-version.sh
@@ -2,9 +2,9 @@
 # OhMySh CLI
 #   !!! You can NOT edit this file !!!
 
-OMS_VER='0.0.3-pre_10'
-OMS_BUILD='10'
-OMS_PRE='pre'
+OMS_VER='0.0.4-build_11'
+OMS_BUILD='11'
+OMS_PRE='build'
 
 OMS_CLI_VER='0.0.3-build_8'
 OMS_CLI_BUILD='8'
