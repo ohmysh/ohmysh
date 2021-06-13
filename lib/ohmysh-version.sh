711bc4fe388424886ff960f868c0599a322ef40b
langonginc <langonginc@yeah.net>
2021-06-13 13:40:08 +0800

    Prepare to 0.0.5-build_12

--- lib/ohmysh-version.sh
+++ lib/ohmysh-version.sh
@@ -2,11 +2,11 @@
 # OhMySh CLI
 #   !!! You can NOT edit this file !!!
 
-OMS_VER='0.0.4-build_11'
-OMS_BUILD='11'
+OMS_VER='0.0.5-build_12'
+OMS_BUILD='12'
 OMS_PRE='build'
 
-OMS_CLI_VER='0.0.3-build_8'
-OMS_CLI_BUILD='8'
+OMS_CLI_VER='0.0.5-build_9'
+OMS_CLI_BUILD='9'
 OMS_CLI_PRE='build'
 
