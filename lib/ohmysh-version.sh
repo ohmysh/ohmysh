7a9813d5b3d1ce1969e937742d5ad709e8f385cf
langonginc <langonginc@yeah.net>
2021-10-06 14:56:59 +0800

    Update to build 16

--- lib/ohmysh-version.sh
+++ lib/ohmysh-version.sh
@@ -2,9 +2,9 @@
 # OhMySh CLI
 #   !!! You can NOT edit this file !!!
 
-OMS_VER='0.0.5-build_15'
-OMS_BUILD='15'
-OMS_PRE='build'
+OMS_VER='0.0.5-build_16'
+OMS_BUILD='16'
+OMS_PRE='PRE'
 
 OMS_CLI_VER='0.0.5-build_10'
 OMS_CLI_BUILD='10'
