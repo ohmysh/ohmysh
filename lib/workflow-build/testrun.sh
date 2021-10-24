760d732e3b7d9c2eedfe8e9a5eb89107b062d8d2
5+1 <59787082+langonginc@users.noreply.github.com>
2021-10-24 17:00:51 +0800

    Update testrun.sh

--- lib/workflow-build/testrun.sh
+++ lib/workflow-build/testrun.sh
@@ -1,15 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
 _path=$(pwd)
 
 mkdir test/cache
 
-sh lib/whatshell.sh
+bash lib/whatshell.sh
 
 OMS_DIR="$path"
 OMS_CACHE="$path/test/cache"
 OMS_THEME='colorshell'
-OMS_PLUGIN=(helloworld )
+OMS_PLUGIN=('helloworld' )
 source "$OMS_DIR/main.sh"
 
 
