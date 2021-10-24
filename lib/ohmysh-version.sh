5a5d827a4d0e88a828a5edc0a0f39c36602ce8d4
5+1 <59787082+langonginc@users.noreply.github.com>
2021-10-24 21:53:46 +0800

    Update to build_18

--- lib/ohmysh-version.sh
+++ lib/ohmysh-version.sh
@@ -2,11 +2,11 @@
 # OhMySh CLI
 #   !!! You can NOT edit this file !!!
 
-OMS_VER='0.0.5-build_PRE17'
-OMS_BUILD='17'
-OMS_PRE='PRE'
+OMS_VER='0.0.5-build_18'
+OMS_BUILD='18'
+OMS_PRE='build'
 
-OMS_CLI_VER='0.0.5-build_11'
-OMS_CLI_BUILD='11'
-OMS_CLI_PRE='PRE'
+OMS_CLI_VER='0.0.5-build_12'
+OMS_CLI_BUILD='12'
+OMS_CLI_PRE='build'
 
