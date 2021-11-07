3b308ea56dd5128573d7d16d7166f508a1cc66a5
langonginc <langonginc@yeah.net>
2021-11-07 20:35:52 +0800

    Update to build_PRE19

--- lib/ohmysh-version.sh
+++ lib/ohmysh-version.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # OhMySh CLI
 #   !!! You can NOT edit this file !!!
-#   build/PRE; x.x.x-build_(PRE)x(.x)
+#   build/PRE; x.x.x-build_(PRE)x
 
-export OMS_VER='0.0.5-build_PRE18.3'
-export OMS_BUILD='18'
+export OMS_VER='0.0.5-build_PRE19'
+export OMS_BUILD='19'
 export OMS_PRE='PRE'
 
 export OMS_CLI_VER='0.0.5-build_PRE13'
