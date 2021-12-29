9a521f2c602367bf28ff80cc51c3e5041d7ce9a9
langonginc <langonginc@yeah.net>
2021-12-29 21:21:47 +0800

    Update to Dec 2021

--- lib/ohmysh-version.sh
+++ lib/ohmysh-version.sh
@@ -3,9 +3,9 @@
 #   !!! You can NOT edit this file !!!
 #   build/PRE; x.x.x-build_(PRE)x
 
-export OMS_VER='0.0.5-build_PRE22'
-export OMS_BUILD='22'
-export OMS_PRE='PRE'
+export OMS_VER='0.0.5-build_23'
+export OMS_BUILD='23'
+export OMS_PRE='build'
 
 export OMS_CLI_VER='0.0.5-build_16'
 export OMS_CLI_BUILD='16'
