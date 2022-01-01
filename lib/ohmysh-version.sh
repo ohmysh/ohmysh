8ccd02cc3e68412a563de0d4f93e1dbbc59319ba
langonginc <langonginc@yeah.net>
2022-01-01 21:24:33 +0800

    Update to 0.0.5-build_PRE24.220101

--- lib/ohmysh-version.sh
+++ lib/ohmysh-version.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
-# OhMySh CLI
+#!/bin/bash
+# OhMySh Version
 #   !!! You can NOT edit this file !!!
-#   build/PRE; x.x.x-build_(PRE)x
+#   build/PRE; x.x.x-build_(PRE)x.yymmdd
 
-export OMS_VER='0.0.5-build_23'
-export OMS_BUILD='23'
-export OMS_PRE='build'
+export OMS_VER='0.0.5-build_PRE24.220101'
+export OMS_BUILD='24'
+export OMS_PRE='PRE'
 
 export OMS_CLI_VER='0.0.5-build_16'
 export OMS_CLI_BUILD='16'
