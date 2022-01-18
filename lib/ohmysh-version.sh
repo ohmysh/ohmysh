7ed243554f3ccd167a07bbd5d27072f51e73047e
langonginc <langonginc@yeah.net>
2022-01-18 15:37:28 +0800

    Update to PRE28

--- lib/ohmysh-version.sh
+++ lib/ohmysh-version.sh
@@ -3,8 +3,8 @@
 #   !!! You can NOT edit this file !!!
 #   build/PRE; x.x.x-build_(PRE)x.yymmdd
 
-export OMS_VER='0.0.5-build_PRE27.220108'
-export OMS_BUILD='27'
+export OMS_VER='0.0.5-build_PRE28.220118'
+export OMS_BUILD='28'
 export OMS_PRE='PRE'
 
 export OMS_CLI_VER='0.0.5-build_17'
