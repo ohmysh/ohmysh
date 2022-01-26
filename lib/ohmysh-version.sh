11078c180921bbd5d08f4cca9fd1c8afda811987
langonginc <langonginc@yeah.net>
2022-01-26 19:01:09 +0800

    RC: Prepare for Jan 2022

--- lib/ohmysh-version.sh
+++ lib/ohmysh-version.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # OhMySh Version
 #   !!! You can NOT edit this file !!!
-#   build/PRE; x.x.x-build_(PRE)x.yymmdd
+#   build/PRE; x.x.x-build_(PRE/RC)x.yymmdd
 
-export OMS_VER='0.0.5-build_PRE28.220118'
-export OMS_BUILD='28'
+export OMS_VER='0.0.5-build_RC29.220126'
+export OMS_BUILD='29'
 export OMS_PRE='PRE'
 
 export OMS_CLI_VER='0.0.5-build_17'
