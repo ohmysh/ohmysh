2efeba1a8b1f77f9d4b28cc18b24e834a8f36945
5+1 <67494254+516wjy@users.noreply.github.com>
2022-01-29 08:39:14 +0800

    Update to Build_30

--- lib/ohmysh-version.sh
+++ lib/ohmysh-version.sh
@@ -3,9 +3,9 @@
 #   !!! You can NOT edit this file !!!
 #   build/PRE; x.x.x-build_(PRE/RC)x.yymmdd
 
-export OMS_VER='0.0.5-build_RC29.220126'
-export OMS_BUILD='29'
-export OMS_PRE='PRE'
+export OMS_VER='0.0.5-build_30.220129'
+export OMS_BUILD='30'
+export OMS_PRE='build'
 
 export OMS_CLI_VER='0.0.5-build_17'
 export OMS_CLI_BUILD='17'
