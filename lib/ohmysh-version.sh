814d7e8228b336be971ac8b4a225878bde940558
langonginc <langonginc@yeah.net>
2022-02-02 16:41:32 +0800

    Update to build_Alpha34

--- lib/ohmysh-version.sh
+++ lib/ohmysh-version.sh
@@ -3,11 +3,11 @@
 #   !!! You can NOT edit this file !!!
 #   build/PRE; x.x.x-build_(Alpha/Beta/RC)x.yymmdd
 
-export OMS_VER='0.0.6-build_Alpha33.220201'
-export OMS_BUILD='33'
+export OMS_VER='0.0.6-build_Alpha34.220202'
+export OMS_BUILD='34'
 export OMS_PRE='PRE'
 
-export OMS_CLI_VER='0.0.6-build_Alpha18.220201'
-export OMS_CLI_BUILD='18'
+export OMS_CLI_VER='0.0.6-build_Alpha19.220202'
+export OMS_CLI_BUILD='19'
 export OMS_CLI_PRE='PRE'
 
