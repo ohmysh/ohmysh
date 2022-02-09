b3a72b808584bd24d5b6c803baa2f63d51c4f561
5+1 <59787082+langonginc@users.noreply.github.com>
2022-02-09 10:10:08 +0800

    Build Alpha36: Cumulative Updates

--- lib/ohmysh-version.sh
+++ lib/ohmysh-version.sh
@@ -3,8 +3,8 @@
 #   !!! You can NOT edit this file !!!
 #   build/PRE; x.x.x-build_(Alpha/Beta/RC)x.yymmdd
 
-export OMS_VER='0.0.6-build_Alpha35.220204'
-export OMS_BUILD='35'
+export OMS_VER='0.0.6-build_Alpha36.220209'
+export OMS_BUILD='36'
 export OMS_PRE='PRE'
 
 export OMS_CLI_VER='0.0.6-build_Alpha21.220207'
