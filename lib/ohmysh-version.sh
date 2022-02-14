c6d2b1c8da8c186d42260a7ffdbba725a29618e7
5+1 <59787082+langonginc@users.noreply.github.com>
2022-02-14 19:01:10 +0800

    Fix

--- lib/ohmysh-version.sh
+++ lib/ohmysh-version.sh
@@ -3,7 +3,7 @@
 #   !!! You can NOT edit this file !!!
 #   build/PRE; x.x.x-build_(Alpha/Beta/RC)x.yymmdd
 
-export OMS_VER='0.0.6-Beta_Alpha37.220214'
+export OMS_VER='0.0.6-build_Beta37.220214'
 export OMS_BUILD='37'
 export OMS_PRE='PRE'
 
