711e701eb3dbe4604a193581a2e317cd633f5c86
5+1 <59787082+langonginc@users.noreply.github.com>
2022-02-14 18:54:01 +0800

    Update to beta

--- lib/ohmysh-version.sh
+++ lib/ohmysh-version.sh
@@ -3,11 +3,11 @@
 #   !!! You can NOT edit this file !!!
 #   build/PRE; x.x.x-build_(Alpha/Beta/RC)x.yymmdd
 
-export OMS_VER='0.0.6-build_Alpha36.220209'
-export OMS_BUILD='36'
+export OMS_VER='0.0.6-Beta_Alpha37.220214'
+export OMS_BUILD='37'
 export OMS_PRE='PRE'
 
-export OMS_CLI_VER='0.0.6-build_Alpha21.220207'
-export OMS_CLI_BUILD='21'
+export OMS_CLI_VER='0.0.6-build_Beta22.220214'
+export OMS_CLI_BUILD='22'
 export OMS_CLI_PRE='PRE'
 
