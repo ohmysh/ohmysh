fd6cc4e1a54c869cfced53a1a8d6091627e05d56
5+1 <59787082+langonginc@users.noreply.github.com>
2022-02-16 11:43:53 +0800

    Update updater

--- lib/update.sh
+++ lib/update.sh
@@ -7,31 +7,38 @@ then
     date +%Y%m%d > "$OMS_CACHE/update"
 fi
 
+
+_oms_update(){
+    _info 'Getting update...' 'Updater'
+    #source $OMS_DIR/lib/ohmysh-version.sh
+    _CACHE_VERSION="$OMS_VER"
+    _CACHE_BUILD="$OMS_BUILD"
+    _CACHE_CLI_VERSION="$OMS_CLI_VER"
+    #_CACHE_CLI_BUILD="$OMS_CLI_BUILD"
+    #_CACHE_PLUGIN=(${OMS_PLUGIN[*]})
+    cd "$OMS_DIR" || exit
+    git pull || _error 'ERROR cannot get update!!!' 'Updater' '6'
+    source "$OMS_DIR/lib/ohmysh-version.sh"
+    date +%Y%m%d > "$OMS_CACHE/update"
+    if [ "$_CACHE_VERSION" != "$OMS_VER" ]
+    then
+        _run_update "$((_CACHE_BUILD+1))" "$OMS_BUILD"
+        _info "Updated OhMySH to version $OMS_VER!" 'Updater'
+        echo "$_CACHE_VERSION" > "$OMS_CACHE/update-lastver"
+        date +%Y%m%d > "$OMS_CACHE/update-lastdate"
+    fi
+    if [ "$_CACHE_CLI_VERSION" != "$OMS_CLI_VER" ]
+        then
+        _info "Updated OhMySH CLI to version $OMS_CLI_VER!" 'Updater'
+    fi
+}
+
+
 if [ "$(cat "$OMS_CACHE/update")" != "$(date +%Y%m%d)" ] || [ -n "$forceUpdate" ]
 then
     if [ -z "$configUpdateDisable" ] || [ "$configUpdate" != 'Disable' ]
     then
-        _info 'Getting update...' 'Updater'
-        #source $OMS_DIR/lib/ohmysh-version.sh
-        _CACHE_VERSION="$OMS_VER"
-        _CACHE_BUILD="$OMS_BUILD"
-        _CACHE_CLI_VERSION="$OMS_CLI_VER"
-        #_CACHE_CLI_BUILD="$OMS_CLI_BUILD"
-        #_CACHE_PLUGIN=(${OMS_PLUGIN[*]})
-        cd "$OMS_DIR" || exit
-        git pull || _error 'ERROR cannot get update!!!' 'Updater' '6'
-        source "$OMS_DIR/lib/ohmysh-version.sh"
-        date +%Y%m%d > "$OMS_CACHE/update"
-        if [ "$_CACHE_VERSION" != "$OMS_VER" ]
-        then
-            _run_update "$((_CACHE_BUILD+1))" "$OMS_BUILD"
-            _info "Updated OhMySH to version $OMS_VER!" 'Updater'
-            echo "$_CACHE_VERSION" > "$OMS_CACHE/update-lastver"
-            date +%Y%m%d > "$OMS_CACHE/update-lastdate"
-        fi
-        if [ "$_CACHE_CLI_VERSION" != "$OMS_CLI_VER" ]
-            then
-            _info "Updated OhMySH CLI to version $OMS_CLI_VER!" 'Updater'
-        fi
+        _oms_update
+#        _oms_update &
     fi
 fi
