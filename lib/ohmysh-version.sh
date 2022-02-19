b3603c36352303912a5b362b1e1ff788a4548f91
5+1 <59787082+langonginc@users.noreply.github.com>
2022-02-19 17:48:08 +0800

    Update to Beta 38: Add reload option

--- lib/ohmysh-version.sh
+++ lib/ohmysh-version.sh
@@ -3,8 +3,8 @@
 #   !!! You can NOT edit this file !!!
 #   build/PRE; x.x.x-build_(Alpha/Beta/RC)x.yymmdd
 
-export OMS_VER='0.0.6-build_Beta37.220214'
-export OMS_BUILD='37'
+export OMS_VER='0.0.6-build_Beta38.220219'
+export OMS_BUILD='38'
 export OMS_PRE='PRE'
 
 export OMS_CLI_VER='0.0.6-build_Beta23.220217'
