f9fca6960c6aeb374a37071b6f4cdb2b505ee6aa
5+1 <59787082+langonginc@users.noreply.github.com>
2022-02-20 19:27:07 +0800

    Update to RC

--- lib/ohmysh-version.sh
+++ lib/ohmysh-version.sh
@@ -3,11 +3,11 @@
 #   !!! You can NOT edit this file !!!
 #   build/PRE; x.x.x-build_(Alpha/Beta/RC)x.yymmdd
 
-export OMS_VER='0.0.6-build_Beta38.220219'
-export OMS_BUILD='38'
+export OMS_VER='0.0.6-build_RC39.220220'
+export OMS_BUILD='39'
 export OMS_PRE='PRE'
 
-export OMS_CLI_VER='0.0.6-build_Beta23.220217'
-export OMS_CLI_BUILD='23'
+export OMS_CLI_VER='0.0.6-build_RC24.220220'
+export OMS_CLI_BUILD='24'
 export OMS_CLI_PRE='PRE'
 
