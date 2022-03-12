8f3acf213cbd6754f0b48078c564e8671af72b46
5+1 <59787082+langonginc@users.noreply.github.com>
2022-03-12 15:28:00 +0800

    alpha43

--- lib/ohmysh-version.sh
+++ lib/ohmysh-version.sh
@@ -3,8 +3,8 @@
 #   !!! You can NOT edit this file !!!
 #   build/PRE; x.x.x-build_(Alpha/Beta/RC)x.yymmdd
 
-export OMS_VER='0.0.7-build_Alpha42.220311'
-export OMS_BUILD='42'
+export OMS_VER='0.0.7-build_Alpha43.220312'
+export OMS_BUILD='43'
 export OMS_PRE='PRE'
 
 export OMS_CLI_VER='0.0.7-build_26.220306'
