73bfd18e3dd6c19c11799ea751ae1afe482f850d
5+1 <59787082+langonginc@users.noreply.github.com>
2022-03-13 15:43:29 +0800

    Alpha44: Add channel select

--- lib/ohmysh-version.sh
+++ lib/ohmysh-version.sh
@@ -3,8 +3,8 @@
 #   !!! You can NOT edit this file !!!
 #   build/PRE; x.x.x-build_(Alpha/Beta/RC)x.yymmdd
 
-export OMS_VER='0.0.7-build_Alpha43.220312'
-export OMS_BUILD='43'
+export OMS_VER='0.0.7-build_Alpha44.220313'
+export OMS_BUILD='44'
 export OMS_PRE='PRE'
 
 export OMS_CLI_VER='0.0.7-build_26.220306'
