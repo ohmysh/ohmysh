ae0cfc0a6ccf1556541c35124e6fd3e7134d9ca2
5+1 <59787082+langonginc@users.noreply.github.com>
2022-05-20 11:17:05 +0800

    0.0.7-build_48.220520

--- lib/ohmysh-version.sh
+++ lib/ohmysh-version.sh
@@ -3,8 +3,8 @@
 #   !!! You can NOT edit this file !!!
 #   build/PRE; x.x.x-build_(Alpha/Beta/RC)x.yymmdd
 
-export OMS_VER='0.0.7-build_47.220430'
-export OMS_BUILD='47'
+export OMS_VER='0.0.7-build_48.220520'
+export OMS_BUILD='48'
 export OMS_PRE='build'
 
 export OMS_CLI_VER='0.0.7-build_29.220430'
