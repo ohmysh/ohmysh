fd2e1e1f064fa9e5b9f41c22ba99c50198fc25f8
5+1 <59787082+langonginc@users.noreply.github.com>
2022-05-22 13:40:32 +0800

    Update to Alpha49

--- lib/ohmysh-version.sh
+++ lib/ohmysh-version.sh
@@ -3,9 +3,9 @@
 #   !!! You can NOT edit this file !!!
 #   build/PRE; x.x.x-build_(Alpha/Beta/RC)x.yymmdd
 
-export OMS_VER='0.0.7-build_48.220520'
-export OMS_BUILD='48'
-export OMS_PRE='build'
+export OMS_VER='0.0.8-build_Alpha49.220521'
+export OMS_BUILD='49'
+export OMS_PRE='PRE'
 
 export OMS_CLI_VER='0.0.7-build_29.220430'
 export OMS_CLI_BUILD='29'
