5f43a5b90cd6717cea5108161a271a1ece8bc3bf
langonginc <langonginc@yeah.net>
2022-07-17 11:17:50 +0800

    Beta51

--- lib/ohmysh-version.sh
+++ lib/ohmysh-version.sh
@@ -3,8 +3,8 @@
 #   !!! You can NOT edit this file !!!
 #   build/PRE; x.x.x-build_(Alpha/Beta/RC)x.yymmdd
 
-export OMS_VER='0.0.8-build_Alpha50.220708'
-export OMS_BUILD='50'
+export OMS_VER='0.0.8-build_Beta51.220717'
+export OMS_BUILD='51'
 export OMS_PRE='PRE'
 
 export OMS_CLI_VER='Not available'
