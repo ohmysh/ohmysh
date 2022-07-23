6ba3677da272ed58bc39efda8122c9fdf6801a8a
5+1 <59787082+langonginc@users.noreply.github.com>
2022-07-23 21:12:58 +0800

    Update title.etc.sh

--- lib/etc/title.etc.sh
+++ lib/etc/title.etc.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # @etc title
 
-# To set the title of The terminal, modify the value of "OMS_TITLE"
+# * To set the title of The terminal, modify the value of "OMS_TITLE".
+# * To disable the function, set the value of "OMS_TITLE" as "Disable".
 
 OMS_TITLE='${pwd##*/}'
 
