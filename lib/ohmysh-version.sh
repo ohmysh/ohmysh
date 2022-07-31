7d9f84ff2bc62cad8b34220cbace364b3f3dcbec
langonginc <langonginc@yeah.net>
2022-07-31 22:08:56 +0800

    build 54: Next update

--- lib/ohmysh-version.sh
+++ lib/ohmysh-version.sh
@@ -3,7 +3,7 @@
 #   !!! You can NOT edit this file !!!
 #   build/PRE; x.x.x-build_(Alpha/Beta/RC)x.yymmdd
 
-export OMS_VER='0.0.8-build_RC53.220731'
-export OMS_BUILD='53'
-export OMS_PRE='PRE'
+export OMS_VER='0.0.8-build_54.220731'
+export OMS_BUILD='54'
+export OMS_PRE='build'
 
