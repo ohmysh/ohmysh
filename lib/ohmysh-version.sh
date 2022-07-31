4ceb7664ab02e95d14d7ef96596e363ba0d88397
langonginc <langonginc@yeah.net>
2022-07-31 10:43:17 +0800

    RC53:Deleted CLI defines, updated docs

--- lib/ohmysh-version.sh
+++ lib/ohmysh-version.sh
@@ -3,11 +3,7 @@
 #   !!! You can NOT edit this file !!!
 #   build/PRE; x.x.x-build_(Alpha/Beta/RC)x.yymmdd
 
-export OMS_VER='0.0.8-build_Beta52.220724'
-export OMS_BUILD='52'
+export OMS_VER='0.0.8-build_RC53.220731'
+export OMS_BUILD='53'
 export OMS_PRE='PRE'
-
-export OMS_CLI_VER='Not available'
-export OMS_CLI_BUILD='0'
-export OMS_CLI_PRE='#'
 
