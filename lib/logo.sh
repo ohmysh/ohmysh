714f58551b79962574ba986b6eca87cd03c41158
0x07CB <83157348+0x07CB@users.noreply.github.com>
2022-08-14 15:03:27 +0000

    logo in rainbow color and 3 time bell sound into terminal

--- lib/logo.sh
+++ lib/logo.sh
@@ -4,18 +4,27 @@ _logo(){
 cat <<EOF
         ____  __   __  ___     ___ _
        / __ \/ /  /  |/  /_ __/ __| |_
-      / /_/ / _ \/ /|_/ / // /\__ \ . \ 
-      \____/_//_/_/  /_/\_, / |___/_||_|
+      / /_/ / _ \/ /|_/ / // /\__ \ . \  
+      \____/_//_/_/  /_/\_, / |___/_||_| [] [] []
                        /___/
+
 EOF
 }
 
 _des(){
-  _logo
+  # play 3 time an bell sound
+  for i in {1..3}; do
+    echo -e "\a"
+  done
+  
+  # print logo in rainbow color
+  _logo | /usr/games/lolcat -a -d 10
+
+
   cat <<EOF
 
-    OhMySh - The Shell Framework
+       OhMySh - The Shell Framework
 EOF
 }
-
-export -f _logo _des
+_des
+#export -f _logo _des
