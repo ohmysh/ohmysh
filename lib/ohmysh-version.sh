8348ff2574835dfc03f5db3150673b513168d6e5
langonginc <langonginc@yeah.net>
2022-09-12 21:23:30 +0800

    update

--- lib/ohmysh-version.sh
+++ lib/ohmysh-version.sh
@@ -3,7 +3,7 @@
 #   !!! You can NOT edit this file !!!
 #   build/PRE; x.x.x-build_(Alpha/Beta/RC)x.yymmdd
 
-export OMS_VER='0.0.10-build_Alpha66.220911'
-export OMS_BUILD='66'
+export OMS_VER='0.0.10-build_Alpha67.220912'
+export OMS_BUILD='67'
 export OMS_PRE='PRE'
 
