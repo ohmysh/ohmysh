307df338c157ff34da4dd72a9240173274e5efab
langonginc <langonginc@yeah.net>
2022-09-24 17:13:53 +0800

    Alpha 69

--- lib/ohmysh-version.sh
+++ lib/ohmysh-version.sh
@@ -3,7 +3,7 @@
 #   !!! You can NOT edit this file !!!
 #   build/PRE; x.x.x-build_(Alpha/Beta/RC)x.yymmdd
 
-export OMS_VER='0.0.10-build_Alpha68.220919'
-export OMS_BUILD='68'
+export OMS_VER='0.0.10-build_Alpha69.220924'
+export OMS_BUILD='69'
 export OMS_PRE='PRE'
 
