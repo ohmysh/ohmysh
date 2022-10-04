d5d52ca718fdca5331b03bbbd621dc491175d75d
langonginc <langonginc@yeah.net>
2022-10-04 08:15:24 +0800

    Update to Beta70

--- lib/ohmysh-version.sh
+++ lib/ohmysh-version.sh
@@ -3,7 +3,7 @@
 #   !!! You can NOT edit this file !!!
 #   build/PRE; x.x.x-build_(Alpha/Beta/RC)x.yymmdd
 
-export OMS_VER='0.0.10-build_Alpha69.220924'
-export OMS_BUILD='69'
+export OMS_VER='0.0.10-build_Beta70.221004'
+export OMS_BUILD='70'
 export OMS_PRE='PRE'
 
