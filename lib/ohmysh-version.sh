51c303279f41f7312f50efd37515ecbff396d118
langonginc <langonginc@yeah.net>
2022-10-23 13:02:14 +0800

    Update to beta71

--- lib/ohmysh-version.sh
+++ lib/ohmysh-version.sh
@@ -3,8 +3,8 @@
 #   !!! You can NOT edit this file !!!
 #   build/PRE; x.x.x-build_(Alpha/Beta/RC)x.yymmdd
 
-export OMS_VER='0.0.10-build_Beta70.221004'
+export OMS_VER='0.0.10-build_Beta71.221023'
 export OMS_VER_NAME='OhMySh Dev (Beta)'
-export OMS_BUILD='70'
+export OMS_BUILD='71'
 export OMS_PRE='PRE'
 
