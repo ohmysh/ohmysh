100ff67c0226193a6b3b1075e08c4b5b402ba9a7
langonginc <langonginc@yeah.net>
2022-10-29 21:27:22 +0800

    RC

--- lib/ohmysh-version.sh
+++ lib/ohmysh-version.sh
@@ -3,8 +3,8 @@
 #   !!! You can NOT edit this file !!!
 #   build/PRE; x.x.x-build_(Alpha/Beta/RC)x.yymmdd
 
-export OMS_VER='0.0.10-build_Beta71.221023'
-export OMS_VER_NAME='OhMySh Dev (Beta)'
-export OMS_BUILD='71'
+export OMS_VER='0.0.10-build_RC72.221029'
+export OMS_VER_NAME='OhMySh Dev (Release Candidate)'
+export OMS_BUILD='72'
 export OMS_PRE='PRE'
 
