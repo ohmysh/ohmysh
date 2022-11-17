a605368fa2471a7d2d985270db348095683ef8ff
langonginc <langonginc@yeah.net>
2022-11-17 15:54:32 +0800

    update to 77

--- lib/ohmysh-version.sh
+++ lib/ohmysh-version.sh
@@ -3,8 +3,8 @@
 #   !!! You can NOT edit this file !!!
 #   build/PRE; x.x.x-build_(Alpha/Beta/RC)x.yymmdd
 
-export OMS_VER='0.0.11-build_Alpha76.221117'
+export OMS_VER='0.0.11-build_Alpha77.221117'
 export OMS_VER_NAME='Dev 0.0.11 (Alpha)'
-export OMS_BUILD='76'
+export OMS_BUILD='77'
 export OMS_PRE='PRE'
 
