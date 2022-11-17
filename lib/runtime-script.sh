43adcd0b8be38ee15ad7500cb27eefa69be01369
langonginc <langonginc@yeah.net>
2022-11-17 11:40:13 +0800

    Fix 41e4733

--- lib/runtime-script.sh
+++ lib/runtime-script.sh
@@ -10,6 +10,7 @@ fi
 #find "$OMS_CACHE/runtime-script" -maxdepth 1 -type f -printf "%f\n" | while IFS= read -r i
 for file in "$OMS_CACHE/runtime-script/"*
 do
+    i="${file##"$OMS_CACHE/runtime-script/"}"
     if [ "${i##*.}" = "sh" ]
     then
         . "${OMS_CACHE}/runtime-script/${i}"
--- lib/startup-script.sh
+++ lib/startup-script.sh
@@ -10,6 +10,7 @@ fi
 # find "$OMS_CACHE/startup-script" -maxdepth 1 -type f -printf "%f\n" | while IFS= read -r i
 for file in "$OMS_CACHE/startup-script/"*
 do
+    i="${file##"$OMS_CACHE/startup-script/"}"
     if [ "${i##*.}" = 'sh' ]
     then
         . "$OMS_CACHE/startup-script/$i"
