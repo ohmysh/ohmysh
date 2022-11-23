bea8d1a45bebd6f05ea91eb3c656de807ca42918
langonginc <langonginc@yeah.net>
2022-11-23 14:49:24 +0800

    update to Beta 79

--- lib/ohmysh-version.sh
+++ lib/ohmysh-version.sh
@@ -3,8 +3,8 @@
 #   !!! You can NOT edit this file !!!
 #   build/PRE; x.x.x-build_(Alpha/Beta/RC)x.yymmdd
 
-export OMS_VER='0.0.11-build_Beta78.221121'
+export OMS_VER='0.0.11-build_Beta79.221123'
 export OMS_VER_NAME='Dev 0.0.11 (Beta)'
-export OMS_BUILD='78'
+export OMS_BUILD='79'
 export OMS_PRE='PRE'
 
