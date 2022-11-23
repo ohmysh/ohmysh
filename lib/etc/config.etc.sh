8fd105bb7e82c00847483ecbac497a5632bd721e
langonginc <langonginc@yeah.net>
2022-11-23 09:18:14 +0800

    update

--- lib/etc/config.etc.sh
+++ lib/etc/config.etc.sh
@@ -7,28 +7,43 @@
 # 'Enable' or 'Disable' are accepted. And also path and values.
 # The default settings are in 'lib/config.sh'
 
+# A. Config
 # configUpdate='Disable'    # Or 'Force' to update fast-forward
 # configCover='Disable'
 # configStartPath="$HOME/"
 # configPathEnv='FULLPATH1:FULLPATH2'
 # configBashcompletion='Enable'
 # configLOLCAT='Disable'
+
+# B. Map
 # mapLs='Disable'
 # mapCd='Disable'
 # mapHelp='Disable'
 # mapMSDos='Disable'
 # mapCSP2021='Disable'
 # mapTrash='Disable'
+# mapGrep='Enable'
+
+# C. Trash
 # trashService='Disable'
 # trashAutoDeleteService='Enable'
 # trashAutoDeleteConfigDate='30'
+
+# D. Completion
 # bashcompletionPlatform='Custom'    # (Auto/Linux/MacOS/Custom)
 # bashcompletionPathLinux='/usr/share/bash-completion/bash_completion'
 # bashcompletionPathMacOS='/sw/etc/bash_completion'
 # bashcompletionPathCustom='Custom path'
+
+# E. Time&Date
 # dateFormat='%x'   # See in 'date --help'
 # timeFormat='%H:%M:%S' # See in 'date --help'
+
+# F. Editor
 # editorSelect='vim'    # Set Editor for editing configuration file.
 
+
+
+# Other
 # OMS_DEBUG=("update")
 # OMS_TITLE="$(pwd)"
