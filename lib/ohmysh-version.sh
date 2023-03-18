512758077b81d00c0be604937d33e53fea539e2d
langonginc <langonginc@yeah.net>
2023-03-18 20:40:51 +0800

    84

--- lib/ohmysh-version.sh
+++ lib/ohmysh-version.sh
@@ -3,8 +3,8 @@
 #   !!! You can NOT edit this file !!!
 #   build/PRE; x.x.x-build_(Alpha/Beta/RC)x.yymmdd
 
-export OMS_VER='0.1.1-build_Alpha83.230112'
+export OMS_VER='0.1.1-build_Alpha84.230318'
 export OMS_VER_NAME='OhMySh 0.1 Dev (Alpha)'
-export OMS_BUILD='83'
+export OMS_BUILD='84'
 export OMS_PRE='PRE'
 
