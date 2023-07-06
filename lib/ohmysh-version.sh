316004414420952b9e1c93321a79844b36aa0e41
5+1 <59787082+langonginc@users.noreply.github.com>
2023-07-06 21:58:19 +0800

    #36 Update to 0.1.1 LTS

--- lib/ohmysh-version.sh
+++ lib/ohmysh-version.sh
@@ -3,8 +3,8 @@
 #   !!! You can NOT edit this file !!!
 #   build/PRE; x.x.x-build_(Alpha/Beta/RC)x.yymmdd
 
-export OMS_VER='0.1.1-build_Alpha84.230318'
-export OMS_VER_NAME='OhMySh 0.1 Dev (Alpha)'
-export OMS_BUILD='84'
-export OMS_PRE='PRE'
+export OMS_VER='0.1.1-build_85.230706'
+export OMS_VER_NAME='OhMySh 0.1 LTS (Jul 2023)'
+export OMS_BUILD='85'
+export OMS_PRE='build'
 
